7a62900364398e5736917b75651dd75c75226f5a
nugusha <nchkhaidze@wpi.edu>
2019-03-13 23:42:48 -0400

    Optimization, modified speed function

--- DinoBotV2.py
+++ DinoBotV2.py
@@ -2,21 +2,20 @@ from PIL import ImageGrab, ImageOps, Image
 import pyautogui
 import time
 import numpy as np
-from ctypes import windll
 from numpy import *
 
 def addtoCoordinate(c, x, y, z, k):
     return (c[0]+x,c[1]+y,c[0]+z,c[1]+k)
 
 class Coordinates():
-    dinosaur = (174, 423)
+    dinosaur = (160, 423)
     d = dinosaur
-    replayBtn = (d[0]+150,d[1]-25)
+    replayBtn = (d[0]+180,d[1])
     flyingdino = addtoCoordinate(d, 60, -10, 100, -5)
-    g = addtoCoordinate(d, 73, -55, 84, -43)
+    g = addtoCoordinate(d, 85, -55, 98, -43)
     c1 = 15
     c2 = 0
-    c3 = [100, 120, 120, 130, 130, 170, 210, 350]
+    #c3 = [100, 120, 120, 130, 130, 170, 210, 350]
     c4 = 25
     
 class times():
@@ -53,13 +52,17 @@ def imageGrab(ind, box=True):
         Y = Coordinates.dinosaur[1]
         cc1 = Coordinates.c1
         cc2 = Coordinates.c2
-        cc3 = (time.clock() - start) + 90 # Coordinates.c3[ind]
+        XX = (time.clock() - start)
+        cc3 = 1.1*XX + 110 # Coordinates.c3[ind]
+        if(XX>60.0):
+            #cc3 += 0.026*(XX-60)**(1.8)
+            cc3 += 0.002*(XX-60)**(2.5)
         cc4 = Coordinates.c4
         box = (X+cc1, Y+cc2, 
             X+cc3, Y+cc4)
         return imageGrabRect(box)
 
-def imageGrabRect(box):
+def imageGrabRect(box, gameover = False):
     image = ImageGrab.grab(box)
     
     arr = np.array(image)
@@ -69,15 +72,20 @@ def imageGrabRect(box):
     count = 0
     fi = None
     la = None
+    blackpixel = 0
     for x in arr:
         la = None
         for y in x:
             if(y[0]<90 and y[1]<90 and y[2]<90):
+                blackpixel += 1
                 if(count == 0):
                     fi = True
                 la = True
                 flag = 0
         count += 1
+
+    if(gameover == True):
+        return abs(blackpixel-67)<17
 
     if(flag == 1):
         return False
@@ -97,12 +105,21 @@ def imageGrabRect(box):
 def main():
     '''
     last = None
+    ccc = 0
     while(True):
         if(last!=pyautogui.position()):
             print(pyautogui.position())
         last=pyautogui.position()
+        ccc += 1
+        if(ccc == 10):
+            break
+        pyautogui.click(Coordinates.dinosaur)
+        pyautogui.click((Coordinates.g[2],Coordinates.g[3]))
+        pyautogui.click(Coordinates.replayBtn)
+    while(True):
+        continue
     '''
-
+ 
     a=0
     b=0
     global start
@@ -116,14 +133,15 @@ def main():
                 ind = i
                 break
  
-        if(imageGrabRect(Coordinates.g)==True):
+        if(imageGrabRect(Coordinates.g, True)==True):
+            pyautogui.click((1000,500))
             
-            end = time.clock()    
+            end = time.clock()
             print(end - start, ind)
             time.sleep(3.0)
-            #pic = pyautogui.screenshot()
-            #r = random.randint(100000, 999999)
-            #pic.save('screenshot'+str(r)+'.png')
+            pic = pyautogui.screenshot()
+            r = random.randint(100000, 999999)
+            pic.save('screenshot'+str(r)+'.png')
                 
             restartGame()
             start = time.clock()
