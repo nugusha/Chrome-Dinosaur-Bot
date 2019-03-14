80576bf7cf30a9930ac62cc9bd54b739270fe986
nugusha <nchkhaidze@wpi.edu>
2019-03-13 20:43:32 -0400

    Optimized jumping as speed goes up

--- DinoBotV2.py
+++ DinoBotV2.py
@@ -14,16 +14,18 @@ class Coordinates():
     replayBtn = (d[0]+150,d[1]-25)
     flyingdino = addtoCoordinate(d, 60, -10, 100, -5)
     g = addtoCoordinate(d, 73, -55, 84, -43)
-    c1 = [15, 15, 15, 15, 15, 15, 15]
-    c2 = [0, 0, 0, 0, 0, 0, 0]
-    c3 = [100, 120, 120, 130, 130, 170, 210]
-    c4 = [25, 25, 25, 25, 25, 25, 25]
+    c1 = 15
+    c2 = 0
+    c3 = [100, 120, 120, 130, 130, 170, 210, 350]
+    c4 = 25
     
 class times():
-    jumplen = [0.18, 0.18, 0.18, 0.17, 0.17, 0.17, 0.17]
-    lendlen = [0.15, 0.15, 0.14, 0.13, 0.12, 0.12, 0.11]
-    ducklen = [0.02, 0.02, 0.02, 0.02, 0.013, 0.012, 0.011]
-    timepassed = [10.0, 20.0, 30.0, 40.0, 70.0, 85.0, 1000.0]
+    jumplen = [0.18, 0.18, 0.18, 0.17, 0.17, 0.17, 0.17, 0.17]
+    lendlen = [0.15, 0.15, 0.14, 0.13, 0.12, 0.12, 0.11, 0.10]
+    ducklen = [0.02, 0.018, 0.017, 0.015, 0.010, 0.010, 0.010, 0.010]
+    timepassed = [10.0, 20.0, 30.0, 40.0, 70.0, 85.0, 100.0, 1000.0]
+
+start = 0.0
  
 def restartGame():
      pyautogui.click(Coordinates.replayBtn)
@@ -32,7 +34,6 @@ def press(st,ind):
     pyautogui.keyDown(st)
     twodino = Coordinates.d + Coordinates.d
     if(st == 'space'):
-        cc3 = Coordinates.c3[ind]
         time.sleep(times.lendlen[ind])
         head = addtoCoordinate(twodino,0,20,50,21)
         
@@ -50,10 +51,10 @@ def imageGrab(ind, box=True):
     if(box==True):
         X = Coordinates.dinosaur[0]
         Y = Coordinates.dinosaur[1]
-        cc1 = Coordinates.c1[ind]
-        cc2 = Coordinates.c2[ind]
-        cc3 = Coordinates.c3[ind]
-        cc4 = Coordinates.c4[ind]
+        cc1 = Coordinates.c1
+        cc2 = Coordinates.c2
+        cc3 = (time.clock() - start) + 90 # Coordinates.c3[ind]
+        cc4 = Coordinates.c4
         box = (X+cc1, Y+cc2, 
             X+cc3, Y+cc4)
         return imageGrabRect(box)
@@ -90,7 +91,7 @@ def imageGrabRect(box):
             return 2
             
         return 0
-                
+                 
     return (flag == 0)
     
 def main():
@@ -104,6 +105,7 @@ def main():
 
     a=0
     b=0
+    global start
     start = time.clock()
     end = None
     restartGame()
