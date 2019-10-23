fc8fc1e153da062cf16b7e35c24a8fe5299ef4a2
nugusha <n.u.g.u.s.h.a.a@gmail.com>
2019-10-23 05:51:26 -0400

    Add results in Database

--- src/db.py
+++ src/db.py
@@ -26,6 +26,12 @@ class Database:
         #for row in self.cur.fetchall() :
         #    print (row[0], " ", row[1])
     
+    def insertResult(self,coef1,coef2,result):
+        command = "INSERT INTO results(coef1, coef2, result) VALUES "
+        command += f"\n ({coef1},{coef2},{result});" 
+        self.cur.execute(command)
+        self.db.commit()
+    
     def select(self):
         command = "SELECT * FROM data"
         self.cur.execute(command)
