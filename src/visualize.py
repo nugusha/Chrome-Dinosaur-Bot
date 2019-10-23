12154b0f21fb58880c01b3c39d681e630eb31e89
nugusha <n.u.g.u.s.h.a.a@gmail.com>
2019-10-23 05:50:33 -0400

    Visualization add penultimate jump points in color magenta

--- src/visualize.py
+++ src/visualize.py
@@ -9,20 +9,27 @@ records = db.select()
 
 recTrue = []
 recFalse = []
+recBeforeFalse = []
 
 for x in records:
     if(x[3]==1):
         recTrue.append(x)
-    else:
+    elif(x[3]==0):
         recFalse.append(x)
+    else:
+        recBeforeFalse.append(x)
 
 
 lengthT = [x[1] for x in recTrue]
 lengthF = [x[1] for x in recFalse]
+lengthBF = [x[1] for x in recBeforeFalse]
+
 speedT = [x[2] for x in recTrue]
 speedF = [x[2] for x in recFalse]
+speedBF = [x[2] for x in recBeforeFalse]
 
 plt.plot(lengthT,speedT,'bo')
 plt.plot(lengthF,speedF,'ro')
+plt.plot(lengthBF,speedBF,'mo')
 
 plt.show()
