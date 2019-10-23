5408006f26272fa0aaa28c6af332cd57c79179c5
nugusha <n.u.g.u.s.h.a.a@gmail.com>
2019-10-23 07:37:35 -0400

    Add SQL script

---
+++ src/SQL_script.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE data;
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `speed` FLOAT(11) NOT NULL,
+  `length` int(11) NOT NULL,
+  `label` int(11) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+  
+DROP TABLE results;
+CREATE TABLE `results` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `coef1` FLOAT(11) NOT NULL,
+  `coef2` FLOAT(11) NOT NULL,
+  `result` int(11) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+  
+SELECT * from data;
+SELECT * from results;
+
+INSERT INTO data(speed, length,label) VALUES (22.04,33,1);
